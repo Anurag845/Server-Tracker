aeaecedcbefeab2567e90c94145916ea6cf03b5e
anurag-g <anuraggujarathi@gmail.com>
2019-10-16 18:58:19 +0530

    commit

--- lib/processes.dart
+++ lib/processes.dart
@@ -11,6 +11,33 @@ class Processes extends StatefulWidget {
 
 class _ProcessesState extends State<Processes> {
 
+  void _showDialog() {
+    
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Terminate?"),
+          content: new Text("Are you sure you want to terminate this process?"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Yes"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            new FlatButton(
+              child: new Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   processPage() {
     return Column(
       children: <Widget>[
@@ -22,33 +49,64 @@ class _ProcessesState extends State<Processes> {
           child: ListView.builder(
             itemCount: widget.processes.length,
             itemBuilder: (BuildContext context, int index) {
-              return Card(
-                child: Wrap(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Column(
+              return Padding(
+                padding: EdgeInsets.all(10),
+                child: InkWell(
+                  onLongPress: () {
+                    _showDialog();
+                  },
+                  child: Card(
+                    elevation: 5,
+                    child: ExpansionTile(
+                      title: Wrap(
                         children: <Widget>[
-                          Text("PID"),
-                          Text(widget.processes[index].pid.toString()),
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Column(
+                              children: <Widget>[
+                                Text("PID"),
+                                Text(widget.processes[index].pid.toString()),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Text(widget.processes[index].datname),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Column(
+                              children: <Widget>[
+                                Text("Duration"),
+                                Text(widget.processes[index].duration),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text("Query"),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text(widget.processes[index].query),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Text(widget.processes[index].datname),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Column(
-                        children: <Widget>[
-                          Text("Duration"),
-                          Text(widget.processes[index].duration),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                )
               );
             },
           )
